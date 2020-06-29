559de5f24dabb540169560b3f8723faa595d374b
brunoartc <brunoartc@yahoo.com>
2020-06-28 21:34:31 -0300

    fix for windows

--- mainsel.py
+++ mainsel.py
@@ -4,17 +4,12 @@ from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 import time
 
+
 profile = FirefoxProfile()
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 'application/ods')
+driver = webdriver.Firefox(firefox_profile=profile)
 
-profile.set_preference("browser.download.panel.shown", False)
-profile.set_preference("browser.helperApps.neverAsk.openFile","text/csv,application/vnd.ms-excel")
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/vnd.ms-excel")
-profile.set_preference("browser.download.folderList", 2);
-profile.set_preference("browser.download.dir", "/home/bruno/Documents/mils")
 
-#binary = FirefoxBinary('./geckodriver')
-
-driver = webdriver.Firefox(profile)
 driver.get('https://sismpconsultapublica.mpsp.mp.br/ConsultarProcedimentos/ObterProcedimentos')
 
 driver.implicitly_wait(3)
@@ -28,7 +23,7 @@ btnbusca.click()
 driver.implicitly_wait(5)
 
 for target_list in range(100):
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(1)
     btnbusca = driver.find_element_by_id('btnConsultar')
     btnbusca = driver.find_elements_by_class_name('botao-paginacao')
     downloadbt = driver.find_element_by_xpath("//form[@action='/ConsultarProcedimentos/DownloadOds']//button")
@@ -38,6 +33,5 @@ for target_list in range(100):
         btnbusca[0].click()
     else:
         btnbusca[-2].click()
-    time.sleep(1)
-    driver.implicitly_wait(5)
+    #time.sleep(1)
 
